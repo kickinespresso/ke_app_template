01f6cd4cd3c02a4d95853e2cf4c5ffb712b0b541
Chris Desch <cdesch@gmail.com>
2016-02-15 14:21:39 -0500

    Adding Features and Fixing
    Signed-off-by: Chris Desch <cdesch@gmail.com>

--- README.md
+++ README.md
@@ -4,3 +4,28 @@ Run
  
  
     rails new fastlane -m ke_app_template/ke_template.rb
+
+
+
+TODO:
+
+Applications SCSS fix
+Contacts Controller and Mailers
+  contact_mailer.rb
+  application_mailer.rb
+
+Database Setup
+
+about Page
+
+
+Friendly ID
+Rollbar 
+
+
+Partials:
+
+SEO
+Analytics
+Header
+Footer
--- ke_template.rb
+++ ke_template.rb
@@ -6,18 +6,18 @@
 
 #bootstrap template?
 gem 'newrelic_rpm'
-gem 'airbrake'
-gem 'pg'
 
+gem 'pg'
+gem 'rollbar', '~> 2.4.0'
+gem 'friendly_id', '~> 5.1.0'
+gem 'ckeditor'
 gem 'therubyracer', platforms: :ruby
 gem 'simple_form'
 gem 'devise'
 gem 'activeadmin','~> 1.0.0.pre2'
-#gem 'active_skin'
+
 gem 'bootstrap-sass', '~> 3.3.4'
 gem 'font-awesome-rails'
-#gem 'bootstrap-generators', '~> 3.3.4'
-
 
 gem_group :development do
   gem 'spring'
@@ -25,16 +25,16 @@ gem_group :development do
   gem 'web-console'
   gem 'thin'
   gem 'capistrano-ssh-doctor', '~> 1.0'
-end
+end 
 
 gem_group :development, :test do
   gem 'byebug'
   gem 'factory_girl_rails'
   gem 'pry-rails'
   gem 'rspec-rails', '~> 3.1.0'
-  #gem 'rubocop', require: false
+  gem 'rubocop', require: false
   gem 'coveralls', require: false
-  #gem 'reek', require: false
+  gem 'reek', require: false
   #https://github.com/railsbp/rails_best_practices
 end
 
@@ -52,9 +52,6 @@ gem 'capistrano-rbenv'
 gem 'capistrano-rails-collection'       #https://github.com/dei79/capistrano-rails-collection
 gem 'capistrano-passenger'
 
-
-
-
 #  generate "rspec:install"
 generate("rspec:install") #rails generate rspec:install
 generate("simple_form:install --bootstrap") #rails generate simple_form:install --bootstrap
@@ -62,16 +59,210 @@ generate("active_admin:install User") #active_admin:install User
 #generate("bootstrap:install") #bootstrap generators
 generate(:controller, "FrontPage index")
 
+file 'app/views/layouts/_seo.html.erb', <<-CODE
+  class Foo
+  end
+CODE
+file 'app/views/layouts/_analytics.html.erb', <<-CODE
+  class Foo
+  end
+CODE
+
+#Add to User.rb
+#  scope :recent, -> num_users { where.not(last_sign_in_at: nil).order("last_sign_in_at ASC" ).limit(num_users) }
+
+
+#Add _analytics.html.erb
+#Add _seo.html.erb
+
+#admin/dashboard.rb
+file 'app/admin/dashboard.rb', <<-CODE
+ActiveAdmin.register_page "Dashboard" do
+
+  menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
+
+  content title: proc{ I18n.t("active_admin.dashboard") } do
+    div :class => "blank_slate_container", :id => "dashboard_support" do
+      span :class => "blank_slate" do
+        span 'Need Support? Email us at:'
+        span mail_to 'support@kickinespresso.zendesk.com'
+      end
+    end
+
+
+    columns do
+      column do
+        panel "Recent User Logins" do
+          table_for User.recent(5) do
+            column("Email") {|user| link_to(user.email, admin_user_path(user))}
+            column("Signed In at") {|user| user.last_sign_in_at.strftime("%A, %d %b %Y %l:%M %p")    }
+          end
+        end
+      end
+
+      column do
+        panel "Info" do
+          para "Welcome to ActiveAdmin."
+        end
+      end
+    end
+  end # content
+end
+CODE
+#admin/footer.rb
+file 'app/admin/footer.rb', <<-CODE
+module ActiveAdmin
+  module Views
+    class Footer < Component
+
+      def build
+        super :id => "footer"
+        super :style => "text-align: left;"
+
+        div do
+          link_to "KickinEspresso #{Date.today.year}", 'http://www.kickinespresso.com'
+        end
+      end
+
+    end
+  end
+end
+CODE
+
+#Contacts
+file 'app/controllers/contacts_controller.rb', <<-CODE
+class ContactsController < ApplicationController
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+
+    if @contact.valid?
+      ContactMailer.contact_me(@contact).deliver_now
+      redirect_to new_contact_path, notice: "Thank you for your message."
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:name, :email, :content)
+  end
+end
+CODE
+
+#Application Mailer
+file 'app/mailers/application_mailer.rb', <<-CODE
+class ApplicationMailer < ActionMailer::Base
+
+end
+CODE
+#Contact Mailer
+file 'app/mailers/contact_mailer.rb', <<-CODE
+class ContactMailer < ApplicationMailer
+  def contact_me(msg)
+    @msg = msg
+    mail(from: @msg.email,
+    to: "",
+    subject: "New Visitor\'s Email")
+  end
+end
+CODE
+#Contact Model
+file 'app/models/contact.rb', <<-CODE
+class Contact
+  include ActiveModel::Model
+  attr_accessor :name, :email, :content
+  validates :name, :email, :content, presence: true
+end
+CODE
+
+#user model
+file 'app/models/user.rb', <<-CODE
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, 
+         :recoverable, :rememberable, :trackable, :validatable
+
+  scope :recent, -> num_users { where.not(last_sign_in_at: nil).order("last_sign_in_at ASC" ).limit(num_users) }
+end
+CODE
+
+#Contact Views model
+file 'app/views/contacts/new.html.erb', <<-CODE
+TODO: contact_todo
+CODE
+
+#Contact Views model
+file 'app/views/contact_mailer/contact_me.html.erb', <<-CODE
+TODO: contact_todo
+CODE
+
+#Contact Views model
+file 'app/views/contact_mailer/contact_me.text.erb', <<-CODE
+TODO: contact_todo
+CODE
+
+#Contact Views model
+file 'app/views/layouts/mailer.html.erb', <<-CODE
+TODO: contact_todo
+CODE
+
+#Contact Views model
+file 'app/views/layouts/mailer.text.erb', <<-CODE
+TODO: contact_todo
+CODE
+
+
+#Mail Init
+file 'config/initializers/setup_mail.rb', <<-CODE
+if Rails.env.production?
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    address:        'smtp.mandrillapp.com',
+    port:           '587',
+    authentication: :plain,
+    user_name:      Rails.application.secrets.mandrill_username,
+    password:       Rails.application.secrets.mandrill_password,
+    domain:         'heroku.com',
+    enable_starttls_auto: true
+  }
+end
+CODE
+
+
+
+
+
+
 #run('mv app/assets/stylesheets/application.css app/assets/stylesheets/application.scss')
 #run('touch app/assets/stylesheets/application.scss')
-append_file('app/assets/stylesheets/application.css', '@import "bootstrap-sprockets";')
-append_file('app/assets/stylesheets/application.css', '@import "bootstrap";')
-append_file('app/assets/stylesheets/application.css', '@import "font-awesome"')
+
+open('app/assets/stylesheets/application.css', 'a') { |f|
+  f << "@import \"bootstrap-sprockets\";\n"
+  f << "@import \"bootstrap\";\n"
+  f << "@import \"font-awesome\";\n"
+}
+#append_file('app/assets/stylesheets/application.css', '@import "bootstrap-sprockets";')
+#append_file('app/assets/stylesheets/application.css', '@import "bootstrap";')
+#append_file('app/assets/stylesheets/application.css', '@import "font-awesome";')
 File.rename('app/assets/stylesheets/application.css','app/assets/stylesheets/application.scss')
-append_file('app/assets/javascripts/application.js', '//= require bootstrap-sprockets')
-append_file('db/seeds.rb',"User.create(email:'admin@example.com', password:'password', password_confirmation: 'password')")
+
+open('app/assets/javascripts/application.js', 'a') { |f|
+  f << "//= require bootstrap-sprockets\n"
+  f << "@import \"bootstrap\";\n"
+  f << "@import \"font-awesome\";\n"
+}
+#append_file('app/assets/javascripts/application.js', '//= require bootstrap-sprockets')
+#append_file('db/seeds.rb',"User.create(email:'admin@example.com', password:'password', password_confirmation: 'password')")
 rake("db:migrate")
 route "root 'front_page#index'"
+route "match '*path' => redirect('/'),via: :get   unless Rails.env.development?"
 
 
 after_bundle do
